cf3dc8bcef5704d16bc4ee10ea7feada5f03a377
danmaevsky <dan.mayevskiy@gmail.com>
2023-03-13 14:17:09 -0400

    Added CORS to restrict access to this API to just the Gateway

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 // Routes
 const changePasswordRouter = require("./routes/changePassword");
@@ -11,12 +12,18 @@ const newTokenRouter = require("./routes/newToken");
 const authRouter = require("./routes/auth");
 const deleteAccountRouter = require("./routes/deleteAccount");
 
+// Instantiating app and restricting communication to only Gateway
 const app = express();
 app.use(express.json());
+app.use(
+    cors({
+        origin: process.env.GATEWAY_ORIGIN,
+    })
+);
 
+// Connecting to MongoDB
 mongoose.set("strictQuery", true);
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to Authentication Database"));
