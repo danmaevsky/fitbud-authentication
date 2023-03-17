4b6206f36845b316c0eaaa246d045a12718bc80a
danmaevsky <dan.mayevskiy@gmail.com>
2023-03-17 00:00:37 -0400

    More expressive logging in rollback.js

--- routes/rollback.js
+++ routes/rollback.js
@@ -4,10 +4,14 @@ const User = require("../models/userSchema");
 const Refresh_Token = require("../models/refreshTokenSchema");
 
 router.post("/", async (req, res) => {
+    console.log("Rolling back...");
     try {
         // delete the user from accounts AND delete the refresh_token too
         await User.findByIdAndDelete(req.body.userId);
+        console.log("User deleted");
         await Refresh_Token.findByIdAndDelete(req.body.userId);
+        console.log("Refresh token deleted");
+        console.log("Roll back successful");
         return res.send({ message: "Account deleted successfully!" });
     } catch (err) {
         console.log(err.message);
