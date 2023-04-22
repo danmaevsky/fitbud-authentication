5c58220bd92eb96519a379922b1b47fc9bcf2ca1
Malav Majmudar <89492231+malav-majmudar@users.noreply.github.com>
2023-04-22 03:02:08 -0400

    Password can now be updated

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require("../models/userSchema");
 
@@ -46,10 +47,70 @@ router.get("/:userId/:token", async (req, res) => {
           .status(200)
           .send({ message: "Token Verified. User may reset password" });
       } catch (err) {
+        if (err instanceof jwt.TokenExpiredError) {
+          res
+            .status(401)
+            .send({ message: "Authentication failed. Access token expired." });
+        }
         res
           .status(401)
-          .send({ message: "Could not verify token. User Unauthorized" });
+          .send({ message: "Authentication failed. Bad credentials." });
       }
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+router.put("/:userId/:token", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      res.status(404).send({ message: "User does not exist" });
+    } else {
+      const resetSecret = FORGOT_PASSWORD_SECRET + user.saltedHashedPass;
+      try {
+        const verifyLink = jwt.verify(req.params.token, resetSecret);
+      } catch (err) {
+        if (err instanceof jwt.TokenExpiredError) {
+          res
+            .status(401)
+            .send({ message: "Authentication failed. Access token expired." });
+        }
+        res
+          .status(401)
+          .send({ message: "Authentication failed. Bad credentials." });
+      }
+      const newPassword = req.body.newPassword;
+      const confirmNewPassword = req.body.confirmNewPassword;
+
+      const passcheck = new RegExp(
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!.@#\$%&\*\.])[A-Za-z0-9!.@#\$%&\*\.]{8,}$/
+      );
+
+      if (!newPassword || !confirmNewPassword) {
+        res.status(400).send({ message: "Missing fields!" });
+      }
+      if (newPassword != confirmNewPassword) {
+        res.status(400).send({ message: "Password fields do not match!" });
+      }
+
+      if (!passcheck.test(newPassword)) {
+        console.log(passcheck.test(newPassword));
+        return res.status(400).send({
+          message:
+            "Password must be at least 8 characters long, contain one uppercase letter, one lowercase letter, one number and one special character!",
+        });
+      }
+      bcrypt.hash(newPassword, 12, (err, hash) => {
+        if (err) {
+          return res.status(500).send({ message: err.message });
+        }
+        user.saltedHashedPass = hash;
+        user.save();
+        console.log("Password changed successfully");
+        return res.status(201).send({ message: "No Content" });
+      });
     }
   } catch (err) {
     res.status(500).send({ message: err.message });
