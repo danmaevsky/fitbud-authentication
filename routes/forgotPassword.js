5a016d569960396f8a91320b162fb761a23fe6da
Malav Majmudar <89492231+malav-majmudar@users.noreply.github.com>
2023-04-23 18:03:15 -0400

    Sends HTML message

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -43,7 +43,7 @@ router.post("/", async (req, res) => {
         from: "fitbudresetpassword@outlook.com",
         to: user.email,
         subject: "Password Reset Link",
-        html: ,
+        html: html,
       };
 
       transporter.sendMail(emailDetails, function (err, info) {
